11c8449244e76cca4830ed97c261121a261fc027
Aldo Hoeben <aldo@fieldofview.com>
2023-03-05 10:31:36 +0100

    Add stub file to extract strings from def.json files

---
+++ getsettingsdefinitiontexts.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from settingspot import write_setting_text
+
+import logging
+
+def main():
+    setting_json_paths = [path for path in self.root_path.rglob("*.def.json") if "test" not in str(path)]
+    for path in setting_json_paths:
+        logging.info(f"Processing {path}...")
+        write_setting_text(path, "i18n-temp")
+
+if __name__=="__main__":
+    main()
