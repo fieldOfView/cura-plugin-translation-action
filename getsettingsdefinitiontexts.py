d4343bbf028e44f0624971d16456db2ce5ee1506
fieldOfView <aldo@fieldofview.com>
2023-03-05 12:00:56 +0100

    Fix passing destination path to write_setting_text

--- getsettingsdefinitiontexts.py
+++ getsettingsdefinitiontexts.py
@@ -6,8 +6,8 @@ import logging
 def main():
     setting_json_paths = [path for path in Path(".").rglob("*.def.json") if "test" not in str(path)]
     for path in setting_json_paths:
-        logging.info(f"Processing {path}...")
-        write_setting_text(path, "i18n-temp")
+        print(f"Processing {path}...")
+        write_setting_text(path, Path("i18n-temp"))
 
 if __name__=="__main__":
     main()
