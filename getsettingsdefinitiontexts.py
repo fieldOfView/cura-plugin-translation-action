5baebfb2032940350a439f89e5a38c7c2da5983f
Aldo Hoeben <aldo@fieldofview.com>
2023-03-05 11:42:27 +0100

    Update getsettingsdefinitiontexts.py

--- getsettingsdefinitiontexts.py
+++ getsettingsdefinitiontexts.py
@@ -4,7 +4,7 @@ from settingspot import write_setting_text
 import logging
 
 def main():
-    setting_json_paths = [path for path in self.root_path.rglob("*.def.json") if "test" not in str(path)]
+    setting_json_paths = [path for path in Path(".").rglob("*.def.json") if "test" not in str(path)]
     for path in setting_json_paths:
         logging.info(f"Processing {path}...")
         write_setting_text(path, "i18n-temp")
